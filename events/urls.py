8a335c840100c7a20d403c461e9dad32a433d73e
itchyboikemmm <168343572+itchyboikemmm@users.noreply.github.com>
2026-05-23 13:45:43 +0800

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import register_event
 
 urlpatterns = [
-    path('register/', register_event, name='register'),
+    path('', register_event, name='register'),
 ]
